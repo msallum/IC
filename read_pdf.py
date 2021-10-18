14a25a2d4fd849fa59db76cfbb5dee3647b0d12b
msallum <miguel_sallum@usp.br>
2021-10-18 11:41:18 -0300

    ref

--- read_pdf.py
+++ read_pdf.py
@@ -7,3 +7,17 @@ Created on Sun Oct 10 15:28:51 2021
 
 import pandas
 import pdfplumber
+import os
+
+os.chdir(r"C:\Users\mig_s\OneDrive\Documentos\GitHub\IC")
+
+estados = {
+    "AC":"Acre",
+    }
+
+file_1 = "ES.pdf"
+cs=pdfplumber.open(file_1)
+
+p_tab=cs.pages[36]
+
+table= p_tab.extract_table()
