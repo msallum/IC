7ee4bf4797f4399e66f75478b34e1a1168fe79ef
msallum <miguel_sallum@usp.br>
2022-01-07 19:18:55 -0300

    finalmente algo

--- read_pdf.py
+++ read_pdf.py
@@ -5,19 +5,24 @@ Created on Sun Oct 10 15:28:51 2021
 @author: mig_s
 """
 
-import pandas
+import pandas as pd
 import pdfplumber
 import os
-
+#%%
 os.chdir(r"C:\Users\mig_s\OneDrive\Documentos\GitHub\IC")
 
-estados = {
-    "AC":"Acre",
-    }
+position = pd.read_excel("Posições.xlsx")
 
-file_1 = "ES.pdf"
+years = ['Município'] + list(range(1991, 2013)) + ['Total']
+
+#%%
+file_1 = "MG.pdf"
 cs=pdfplumber.open(file_1)
+data = pd.DataFrame()
+p_tab=cs.pages[36:41]
+for i in range(len(p_tab)):
+    table= p_tab[i].extract_table()
+    [row.pop() for row in table]
+    df=pd.DataFrame(table[2:], columns= years).replace('', 0)
+    data = data.append(df, True)
 
-p_tab=cs.pages[36]
-
-table= p_tab.extract_table()
