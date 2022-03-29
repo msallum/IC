25cf2688980e7dc521e2403d438b2ec3c5246405
msallum <miguel_sallum@usp.br>
2022-03-29 13:53:04 -0300

    atualizações pacaa

--- read_pdf.py
+++ read_pdf.py
@@ -14,6 +14,7 @@ os.chdir(r"C:\Users\mig_s\OneDrive\Documentos\GitHub\IC")
 ibge = pd.read_excel(r"RELATORIO_DTB_BRASIL_MUNICIPIO.xls")
 
 ibge["Município"]= ibge["Nome_Município"].apply(lambda x: x.upper())
+ibge["Código Município Completo"] = ibge["Código Município Completo"].astype(str)
 ibge = ibge[["Nome_UF", "Código Município Completo", "Município"]]
 
 position = pd.read_excel("Posições.xlsx")
@@ -22,10 +23,10 @@ posi_dict =  position.to_dict()
 years = ['Município'] + list(range(1991, 2013)) + ['Total']
 
 #%%
-file_1 = "SP.pdf"
+file_1 = "PA.pdf"
 cs=pdfplumber.open(file_1)
 data = pd.DataFrame()
-p_tab = cs.pages[33:35]
+p_tab = cs.pages[43:44]
 for i in range(len(p_tab)):
     table= p_tab[i].extract_table()
     [row.pop() for row in table]
@@ -34,7 +35,7 @@ for i in range(len(p_tab)):
 temp= ibge[ibge["Nome_UF"] == "São Paulo"]
 data.merge(temp, on="Município")
 #%%
-data = dict()
+data_2 = dict()
 for i in range(26):
     if posi_dict["pdf"][i] == "PA":
         continue
@@ -60,10 +61,26 @@ for i in range(26):
                         [row.pop() for row in table]
                     df_sub = pd.DataFrame(table[2:], columns= years).replace('', 0)
                     df = df.append(df_sub, True)
+            elif pd.isnull(posi_dict[j][i]):
+                df = pd.DataFrame(columns= years)
             temp= ibge[ibge["Nome_UF"] == posi_dict["Estado"][i]]
-            df = df.merge(temp, on = "Município")
+            df = pd.to_numeric(df)
             #name = posi_dict["Estado"][i] + "_" + j
-            data_1[j] = df
-    data[posi_dict["Estado"][i]] = data_1
-                
-                
+            data_1[j] = df.merge(temp, how = "outer" , on = "Município")
+    data_2[posi_dict["Estado"][i]] = data_1
+   
+#%%
+data = dict()
+for j in data_2['Acre'].keys():
+    des = pd.DataFrame()
+    for i in data_2.keys():
+        des = des.append(data_2[i][j], True)
+    des = des.drop(des[des['Município'].str.isnumeric().fillna(True)].index)
+    des[list(range(1991, 2013))] = des[list(range(1991, 2013))].apply(pd.to_numeric)
+    data[j] = des.fillna(0)
+       
+#%%
+
+with pd.ExcelWriter(r'Dados limpos/Dados_limpos.xlsx') as writer:
+    for i in data.keys():
+        data[i].to_excel(writer, sheet_name=i)
