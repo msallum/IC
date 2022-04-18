fc43145e3e7af64f68096a974631cbb6a4e8e462
msallum <miguel_sallum@usp.br>
2022-04-18 09:04:45 -0300

    bora lá

--- estruturação_out.R
+++ estruturação_out.R
@@ -1,13 +1,35 @@
 library(haven)
 library(tidyverse)
+library(readxl)
+library(magrittr)
 
 
-setwd("C:/Users/mig_s/OneDrive/Documentos/GitHub/IC/")
+setwd("C:/Users/mig_s/OneDrive/Documents/Dados IC")
+
+read_excel_allsheets <- function(filename, tibble = TRUE) {
+  sheets <- readxl::excel_sheets(filename)
+  x <- lapply(sheets, function(X) readxl::read_excel(filename, sheet = X))
+  if(!tibble) x <- lapply(x, as.data.frame)
+  names(x) <- sheets
+  x
+}
 
 migr <- as_tibble(read_dta("mig_orig_dest.dta"))
 amc <- as_tibble(read_dta("amcs1991.dta"))
+desastres <- as_tibble(read_excel_allsheets("Dados Limpos/Testes.xlsx"))
+
+tib <-enframe(desastres$`Estiagem e Seca`$match, name = "id", value = "match")
+for (a in names(desastres)){
+  desastres[[a]] %>%
+    pivot_longer(c(`1991`:`2012`), names_to = a)%>%
+    full_join(tib, by = "match") -> tib
+}
+  
+  
 
 migr %>%
   group_by(amc_orig, year) %>%
   summarise(outmigration = sum(mig)) %>%
   saveRDS(file = "Dados limpos/outmigration.rds")
+
+
--- read_pdf.py
+++ read_pdf.py
@@ -1,21 +1,28 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Oct 10 15:28:51 2021
+Created on Tue Apr 12 15:23:15 2022
 
 @author: mig_s
 """
-
+#"SANTARÉM = JOCA CLAUDINO(PARAÍBA)
+#AUGUSTO SEVERO = CAMPO GRANDE(RIO GRANDE DO NORTE)
+#PARANÁ ESTIAGEM E SECA TÁ CAGADO
+#PG50 MARANHÃO TBM
+#BAHIA INUNDAÇÃO
 import pandas as pd
 import pdfplumber
 import os
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
+from datetime import datetime
 #%%
 os.chdir(r"C:\Users\mig_s\OneDrive\Documents\Dados IC")
 
 ibge = pd.read_excel(r"RELATORIO_DTB_BRASIL_MUNICIPIO.xls")
 
-ibge["Município"]= ibge["Nome_Município"].apply(lambda x: x.upper())
+ibge["match"]= ibge["Nome_Município"].apply(lambda x: x.upper())
 ibge["Código Município Completo"] = ibge["Código Município Completo"].astype(str)
-ibge = ibge[["Nome_UF", "Código Município Completo", "Município"]]
+ibge = ibge[["Nome_UF", "Código Município Completo", "match"]]
 
 position = pd.read_excel("Posições.xlsx")
 posi_dict =  position.to_dict()
@@ -26,50 +33,127 @@ years = ['Município'] + list(range(1991, 2013)) + ['Total']
 #%%
 data_2 = dict()
 for i in range(26):
-    if posi_dict["pdf"][i] == "PA":
-        continue
-    file = posi_dict["pdf"][i]+".pdf"
     data_1 = dict()
-    cs=pdfplumber.open(file)
-    for j in posi_dict.keys():
-        if j != "pdf" and j != "Estado" and j != "Diagnóstico":
-            if isinstance(posi_dict[j][i], int):
-                p_tab = cs.pages[posi_dict[j][i]]
-                table = p_tab.extract_table()
-                if len(table[0]) > 24:
-                    [row.pop() for row in table]
-                df = pd.DataFrame(table[2:], columns= years).replace('', 0)
-            elif isinstance(posi_dict[j][i], str):
-                pg = posi_dict[j][i].split("-")
-                pg = list(map(int, pg))
-                p_tab = cs.pages[pg[0]:pg[1]]
-                df = pd.DataFrame()
-                for k in range(len(p_tab)):
-                    table= p_tab[k].extract_table()
+    if posi_dict["pdf"][i] == "PA":
+        for j in posi_dict.keys():
+            if j != "pdf" and j != "Estado" and j != "Diagnóstico":
+                try:
+                    data_1[j] = pd.read_excel("PA.xlsx", j)
+                except ValueError:
+                    data_1[j] = pd.DataFrame(columns= years)
+    else:
+        file = posi_dict["pdf"][i]+".pdf"
+        cs=pdfplumber.open(file)
+        for j in posi_dict.keys():
+            if j != "pdf" and j != "Estado" and j != "Diagnóstico":
+                if isinstance(posi_dict[j][i], int):
+                    p_tab = cs.pages[posi_dict[j][i]]
+                    table = p_tab.extract_table()
                     if len(table[0]) > 24:
                         [row.pop() for row in table]
-                    df_sub = pd.DataFrame(table[2:], columns= years).replace('', 0)
-                    df = df.append(df_sub, True)
-            elif pd.isnull(posi_dict[j][i]):
-                df = pd.DataFrame(columns= years)
-            temp= ibge[ibge["Nome_UF"] == posi_dict["Estado"][i]]
-            #df = pd.to_numeric(df)
-            #name = posi_dict["Estado"][i] + "_" + j
-            data_1[j] = df.merge(temp, how = "outer" , on = "Município")
+                    df = pd.DataFrame(table[2:], columns= years).replace('', 0)
+                elif isinstance(posi_dict[j][i], str):
+                    pg = posi_dict[j][i].split("-")
+                    pg = list(map(int, pg))
+                    p_tab = cs.pages[pg[0]:pg[1]]
+                    df = pd.DataFrame()
+                    for k in range(len(p_tab)):
+                        if posi_dict["pdf"][i] == "MA" and k == 39:
+                            df_sub = pd.read_excel() 
+                        else:
+                            table= p_tab[k].extract_table()
+                            if len(table[0]) > 24:
+                                [row.pop() for row in table]
+                            df_sub = pd.DataFrame(table[2:], columns= years).replace('', 0)
+                        df = df.append(df_sub, True)
+                elif pd.isnull(posi_dict[j][i]):
+                    df = pd.DataFrame(columns= years)
+                #temp= ibge[ibge["Nome_UF"] == posi_dict["Estado"][i]]
+                #df = pd.to_numeric(df)
+                #name = posi_dict["Estado"][i] + "_" + j
+                if posi_dict["pdf"][i] == "PB":
+                    df.loc[df['Município']== "SANTARÉM", 'Município'] = "JOCA CLAUDINO"
+                if posi_dict["pdf"][i] == "RN":
+                    df.loc[df['Município']== "AUGUSTO SEVERO", 'Município'] = "CAMPO GRANDE"
+                data_1[j] = df.drop(df[df['Município'].str.isnumeric().fillna(True)].index).drop_duplicates()
     data_2[posi_dict["Estado"][i]] = data_1
-   
+    
+#%%
+
+threshold = 70
+
+for i in data_2.keys():
+    if i == "Goiás":
+        temp = ibge[(ibge["Nome_UF"] == i) | (ibge["Nome_UF"] == "Distrito Federal")]
+    else:
+        temp = ibge[ibge["Nome_UF"] == i]
+    for j in data_2[i].keys():
+        mat1 = []
+        mat2 = []
+        mat3 = []
+        mat4 = []
+        mat5 = []
+        p = []
+        p1 = []
+    
+        list1 = data_2[i][j]['Município'].tolist()
+        list2 = temp['match'].tolist()
+        
+        
+        dt = datetime.now()
+        
+        print(i+"-"+j)
+        print(dt)
+
+
+  
+        # iterating through list1 to extract 
+        # it's closest match from list2
+        for k in list1:
+            mat1.append(process.extract(k, list2, limit=2))
+            mat2.append(process.extractOne(k, list2))
+        data_2[i][j]['matches'] = mat1
+        data_2[i][j]['match'] = mat2
+          
+        # iterating through the closest matches
+        # to filter out the maximum closest match
+        for f in data_2[i][j]['matches']:
+            for k in f:
+                p.append(k[0])
+            mat3.append(",".join(p))
+            p = []
+            
+        for k in data_2[i][j]['match']:
+            if k[1] >= threshold:
+                mat4.append(k[0])
+                mat5.append(k[1])
+            else:
+                mat4.append(0)
+                mat5.append(0)
+            
+
+            
+          
+        # storing the resultant matches back to dframe1
+        data_2[i][j]['matches'] = mat3
+        data_2[i][j]['match'] = mat4
+        data_2[i][j]['fit'] = mat5
+        data_2[i][j]['Nome_UF'] = i
+        #data_2[i][j] = data_2[i][j].merge(temp, how = "left", on=('match', 'Nome_UF'))
+
+
 #%%
 data = dict()
 for j in data_2['Acre'].keys():
     des = pd.DataFrame()
     for i in data_2.keys():
         des = des.append(data_2[i][j], True)
-    des = des.drop(des[des['Município'].str.isnumeric().fillna(True)].index)
+    des = des.drop(des[des['match'].str.isnumeric().fillna(True)].index)
     des[list(range(1991, 2013))] = des[list(range(1991, 2013))].apply(pd.to_numeric)
     data[j] = des.fillna(0)
        
 #%%
 
-with pd.ExcelWriter(r'Dados limpos/Desastres.xlsx') as writer:
+with pd.ExcelWriter(r'Dados limpos/Testes_1.xlsx') as writer:
     for i in data.keys():
         data[i].to_excel(writer, sheet_name=i)
