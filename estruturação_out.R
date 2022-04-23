df2f2faa5903d3173082bdab8df19307853010e7
msallum <miguel_sallum@usp.br>
2022-04-23 19:19:29 -0300

    Update estruturação_out.R

--- estruturação_out.R
+++ estruturação_out.R
@@ -35,22 +35,8 @@ tib <- IBGE %>%
          `Região Geográfica Intermediária`,
          `Código Município Completo`) %>%
   mutate_all(toupper)
-#tib %<>%
-#  full_join(amc, by = c("Código Município Completo" = "cod2010"))
-transf <- function(df, name ,cod = tib){
-  df %>%
-    mutate(Nome_UF = toupper(Nome_UF))%>%
-    select(`1991`:`2012`, match, Nome_UF)%>%
-    full_join(cod, by = c("match", "Nome_UF")) %>%
-    pivot_longer(c(`1991`:`2012`), names_to = "ANO", values_to = name) %>%
-    mutate_if(is.numeric, ~replace(., is.na(.), 0)) %>%
-    as_tibble()
-}
-mergesim <- function(x, y, co, b = NULL){
-  y2 <- y[, ..co]
-  merge.data.table(x,y2, by = b)
-}
-transf2 <- function(df, name, cod = tib){
+
+transf <- function(df, name, cod = tib){
   df%>%
     mutate(desastre = name,
            Nome_UF = toupper(Nome_UF))%>%
@@ -60,18 +46,14 @@ transf2 <- function(df, name, cod = tib){
 }
 
   
-desastres  %>%
+
+
+
+desastres %<>%
   map2(., names(.), transf)%>%
-  reduce(function(x,y){full_join(x,y,by =c("match", "Nome_UF"))})
-
-
-desastres %>%
-  map2(., names(.), transf2)%>%
   bind_rows() %>%
-  pivot_longer(c(`1991`:`2012`), names_to = "ANO") -> desastre1
-
-%>% 
-  pivot_wider(names_from = desastre, values_from = value, values_fill = 0)
+  pivot_longer(c(`1991`:`2012`), names_to = "ANO") %>% 
+  pivot_wider(names_from = desastre, values_from = value)
 
 migr %>%
   group_by(amc_orig, year) %>%
